0ceb3ac81008727663876145fd451cbae83c8bb9
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-26 15:25:56 +0100

    added columns into technologies table

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
+use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +12,13 @@ class TechnologySeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+        $labels = ['HTML', 'CSS', 'Bootstrap', 'Javascript', 'Vue', 'PHP', 'Laravel'];
+        foreach ($labels as $label) {
+            $technology = new Technology();
+            $technology->label = $label;
+            $technology->color = $faker;
+        }
     }
 }
